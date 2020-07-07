3e33e53ae9a1fedca8f601ab90eb4834930e7c8d
Kathlia Carlos <lia.carlos23@gmail.com>
2020-07-07 14:07:18 +0800

    add main functions needed for gameboard

---
+++ script.js
@@ -0,0 +1,9 @@
+
+const GameBoard = {
+    setPlayer1: "factor function",
+    // setPlayer2: "factory func",
+    // setP1marker: ,
+    // setP2marker: ,
+    // addScoreP1: ,
+    // addScoreP2: ,
+}
